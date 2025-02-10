634b3ff79e66d34e1efeb0c57d269c480baac6f6
Andrew Smith <meuzishun@gmail.com>
2025-02-10 07:37:51 -0500

    Complete 'Convert a String to a Number' challenge

---
+++ 8kyu/convert_a_string_to_a_number.ts
@@ -0,0 +1,25 @@
+// My solution
+
+export function stringToNumber(str: string): number {
+  return +str;
+}
+
+/*
+Parameters: a string
+Return: a number
+Examples:
+    stringToNumber("1234") -> 1234
+    stringToNumber("605") -> 605
+    stringToNumber("1405") -> 1405
+    stringToNumber("-7") -> -7
+Pseudocode:
+  Number(str)
+  or
+  +str
+*/
+
+// Top solution
+
+// export function stringToNumber(str: string): number {
+//   return Number(str);
+// }
