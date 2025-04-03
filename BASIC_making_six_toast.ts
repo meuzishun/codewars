2f73400bc3adcfe600e479577fd38426afa66642
Andrew Smith <meuzishun@gmail.com>
2025-04-03 09:00:46 -0400

    Complete 'BASIC Making Six Toast' challenge

---
+++ BASIC_making_six_toast.ts
@@ -0,0 +1,18 @@
+// My solution
+
+export function sixToast(num: number): number {
+  return Math.abs(6 - num);
+}
+
+/*
+Parameters: a number
+Return: a number
+Examples:
+    sixToast(15) -> 9
+    sixToast(6) -> 0
+    sixToast(3) -> 3
+Pseudocode:
+  return the absolute value of 6 minus num
+*/
+
+// Top solutions the same.
