987ef7caefc04a58231aac6bc377341a7cb995b3
Andrew Smith <meuzishun@gmail.com>
2025-04-18 05:08:47 -0400

    Complete 'Register for the Party SQL for Beginners #3' challenge

---
+++ 8kyu/Register_for_the_Party_SQL_for_Beginners_3.sql
@@ -0,0 +1,14 @@
+-- My solution
+
+INSERT INTO participants
+  (name, age, attending)
+VALUES
+  ('Andrew Smith', 43, true);
+
+SELECT * FROM participants;
+
+-- Top solution
+
+INSERT INTO participants VALUES ('mozart', '26', true);
+
+SELECT * FROM participants;
